b5c12637b7b02d2d8925933e027a2b88832f6f72
Callum Marshall <callummarshall@localhost.localdomain>
2019-05-22 21:48:38 +0100

    Added verbose output

--- build_number.h
+++ build_number.h
@@ -1,9 +1,9 @@
 #ifndef BUILD_NUMBER_STR
-#define BUILD_NUMBER_STR "26"
+#define BUILD_NUMBER_STR "28"
 #endif
 #ifndef VERSION_STR
-#define VERSION_STR "126 - Wed 22 May 21:43:06 BST 2019"
+#define VERSION_STR "128 - Wed 22 May 21:46:33 BST 2019"
 #endif
 #ifndef VERSION_STR_SHORT
-#define VERSION_STR_SHORT "126"
+#define VERSION_STR_SHORT "128"
 #endif
--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ int main(int argc, char** argv) {
 	int scale = 1;
 	int runs = 200 * scale * scale * scale;
 	for(int i = 0; i < runs; i = i + 1) {
-		std::cout << "Saving data - " << i  << "/" << runs << std::endl;
+		float percentage = (float)(i+1) / (float)(runs) * 100.0;
+		std::cout << "Saving data - " << (i+1)  << "/" << runs << " (" << percentage << "%)" << std::endl;
 		solver.output_lbm_data("output/" + std::to_string(i) + ".csv");
 		std::cout << "Performing timestep - " << i << "/" << runs << std::endl;
 		solver.perform_timestep();
